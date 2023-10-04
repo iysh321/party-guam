5495f6109a5adac278c2796bae439765cdf1bec4
Jung InYong <iysh321@gmail.com>
2023-10-04 23:27:51 +0900

    feat: login 로직 추가

--- src/users/user/interface/user.controller.ts
+++ src/users/user/interface/user.controller.ts
@@ -1,11 +1,15 @@
+import { LoginCommand } from '../application/command/login.command';
 import { Body, Controller, Post } from '@nestjs/common';
 
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateUserCommand } from '../application/command/create-user.command';
 
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UserLoginDto } from './dto/user-login.dto';
 
-@Controller('user')
+@ApiTags('users')
+@Controller('users')
 export class UserController {
   constructor(
     private commandBus: CommandBus,
@@ -13,10 +17,21 @@ export class UserController {
   ) {}
 
   @Post()
+  @ApiOperation({ summary: '회원가입' })
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
     const { name, email } = dto;
 
     const command = new CreateUserCommand(name, email);
+
+    return this.commandBus.execute(command);
+  }
+
+  @Post('/login')
+  @ApiOperation({ summary: '로그인' })
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    const { email } = dto;
+
+    const command = new LoginCommand(email);
 
     return this.commandBus.execute(command);
   }
